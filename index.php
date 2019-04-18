174923f756fa1a6405bd99abc0783506ec18956b
SaraLiWijnia <saraliwynia@gmail.com>
2019-04-18 14:45:31 +0200

    Basis menu bar

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>EenmaalAndermaal</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-</head>
-<body>
-    <header>
-        EenmaalAndermaal
-    </header>
-    <main>
+<?php include 'template/header.php'; ?>
 
-    </main>
-    <footer>
 
-    </footer>
-</body>
-</html>
+<?php include 'template/footer.php'; ?>
---
+++ template/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>EenmaalAndermaal</title>
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/bootstrap/css/style.css">
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-default navbar-light bg-light"> 
+    <header class="navbar-header">
+      <a class="navbar-brand" href="#"><img class="logo" src="assets/images/alleen-logo.png" alt="logo EenmaalAndermaal"></a>
+    </header>
+    
+    <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+    </nav>
+    </header>
+    <main>
