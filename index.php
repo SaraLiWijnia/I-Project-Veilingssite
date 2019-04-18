92d30bf5816ce338b5ccc6ba130668348a90965a
Mark Brouwer <mbbrou@gmail.com>
2019-04-18 11:11:06 +0200

    Added index file with bootstrap

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>EenmaalAndermaal</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        EenmaalAndermaal
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
