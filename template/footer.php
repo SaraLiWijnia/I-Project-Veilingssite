fadc52a67a9db0f8c3a8f5d18dac2f834e801a23
SaraLiWijnia <saraliwynia@gmail.com>
2019-04-21 20:13:06 +0200

    begin header
    begin header gemaakt. niet af

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,10 @@
 </main>
-    <footer>
+<footer>
 
-    </footer>
+</footer>
+<script type="text/javascript" src="./assets/bootstrap/js/jquery-3.4.0.min.js"></script>
+<script type="text/javascript" src="./assets/bootstrap/js/bootstrap.bundle.min.js"> </script>
+
 </body>
+
 </html>
--- template/header.php
+++ template/header.php
@@ -1,22 +1,42 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>EenmaalAndermaal</title>
-    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/bootstrap/css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>EenmaalAndermaal</title>
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/bootstrap/css/style.css">
+
 </head>
+
 <body>
-    <header>
-    <nav class="navbar navbar-default navbar-light bg-light"> 
-    <header class="navbar-header">
-      <a class="navbar-brand" href="#"><img class="logo" src="assets/images/alleen-logo.png" alt="logo EenmaalAndermaal"></a>
-    </header>
-    
-    <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+  <header>
+    <nav class="navbar navbar-default navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img class="logo" src="assets/images/alleen-logo.png" alt="logo EenmaalAndermaal" width="70" height="70">
+        EenmaalAndermaal
+      </a>
+
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+      <div class="btn-group">
+        <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          Large button
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
+
+
+      </div>
     </nav>
-    </header>
-    <main>
+  </header>
+
+  <main>
