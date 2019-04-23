de9f6b8c45abe36aa493b439c640a0e0fc3f6dd2
Ginochi <47328006+Ginochi@users.noreply.github.com>
2019-04-23 11:09:03 +0200

    Navbar edit 1.1

--- template/header.php
+++ template/header.php
@@ -2,41 +2,42 @@
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>EenmaalAndermaal</title>
-  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./assets/bootstrap/css/style.css">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>EenmaalAndermaal</title>
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/fontawesome/css/all.css">
+    <link rel="icon" href="assets\images\alleen-logo-icon.ico">
 </head>
 
 <body>
-  <header>
-    <nav class="navbar navbar-default navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img class="logo" src="assets/images/alleen-logo.png" alt="logo EenmaalAndermaal" width="70" height="70">
-        EenmaalAndermaal
-      </a>
+    <header>
+        <nav class="navbar navbar-default navbar-light bg-light">
+            <a class="navbar-brand" href="index.php">
+                <img class="logo" src="assets/images/alleen-logo.png" alt="logo EenmaalAndermaal" width="70" height="70">
+                EenmaalAndermaal
+            </a>
 
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Zoeken" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            </form>
 
-      <div class="btn-group">
-        <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          Large button
-        </button>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+            <div class="btn-group">
+                <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Mijn Account
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Mijn Gegevens</a>
+                    <a class="dropdown-item" href="#">Mijn Veilingen</a>
+                    <a class="dropdown-item" href="#">Mijn Biedingen</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Uitloggen</a>
+                </div>
+                <button class="btn btn-sm btn-outline-secondary" type="button"><i class="far fa-bell fa-2x"></i></button>
+            </div>
+        </nav>
+    </header>
 
-
-      </div>
-    </nav>
-  </header>
-
-  <main>
+    <main>
